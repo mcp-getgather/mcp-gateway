dce34f1d4cded87a2c0338b85995034eaa574764
Bin Jiang <bin.jiang@heyario.com>
2025-09-18 13:57:40 -0700

    set long timeout for proxy read operation

--- src/settings.py
+++ src/settings.py
@@ -18,6 +18,9 @@ class Settings(BaseSettings):
     OAUTH_GITHUB_CLIENT_SECRET: str = ""
     OAUTH_GITHUB_REDIRECT_PATH: str = "/auth/github/callback"
 
+    PROXY_TIMEOUT: float = 10.0  # timeout for general operations
+    PROXY_READ_TIMEOUT: float = 120.0  # long timeout for read operations
+
     @property
     def auth_provider(self) -> str:
         """Only supports GitHub for now."""
