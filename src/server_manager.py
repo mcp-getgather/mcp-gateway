289369acd2b79f0a6299f2c04444421f983a59c2
Bin Jiang <bin.jiang@heyario.com>
2025-10-10 10:49:39 -0700

    fix assign_container

--- src/server_manager.py
+++ src/server_manager.py
@@ -189,9 +189,6 @@ class ServerManager:
         if user:
             network_aliases.append(cls.external_hostname(user.user_id))
 
-        # tailscale router is needed to access proxy service
-        tailscale_router_ip = f"{settings.DOCKER_SUBNET_PREFIX}.2"
-
         config: dict[str, Any] = {
             "Image": settings.SERVER_IMAGE,
             "Env": [
@@ -215,12 +212,11 @@ class ServerManager:
         # so we need to override the entrypoint to install iproute2 and add tailscale routing.
         # The container also needs NET_ADMIN capabilities
         if platform.system() != "Darwin":
-            tailscale_router_ip = f"{settings.DOCKER_SUBNET_PREFIX}.2"
             config.update({
                 "Entrypoint": ["/bin/sh", "-c"],
                 "Cmd": [
                     "apt update && apt install -y iproute2 &&"
-                    f" ip route add 100.64.0.0/10 via {tailscale_router_ip} &&"
+                    f" ip route add 100.64.0.0/10 via {cls._tailscale_router_ip()} &&"
                     " exec /app/entrypoint.sh"
                 ],
             })
@@ -254,6 +250,17 @@ class ServerManager:
                     ]
                 },
             })
+
+            if platform.system() != "Darwin":
+                await container.container.exec([
+                    "ip",
+                    "route",
+                    "add",
+                    "100.64.0.0/10",
+                    "via",
+                    cls._tailscale_router_ip(),
+                ])
+
             logger.info(f"Assigned container {container.id} to {user.user_id}")
 
         return user.user_id
@@ -277,6 +284,11 @@ class ServerManager:
     @classmethod
     def _network_name(cls):
         return f"{settings.DOCKER_PROJECT_NAME}_{settings.DOCKER_NETWORK_NAME}"
+
+    @classmethod
+    def _tailscale_router_ip(cls):
+        """IP address of the tailscale router for accessing proxy service."""
+        return f"{settings.DOCKER_SUBNET_PREFIX}.2"
 
     @classmethod
     def _user_server_host(cls, user: AuthUser) -> str:
